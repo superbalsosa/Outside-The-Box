d7cb3d9cb0652109f6def6663bf459a97f59baf8
julianKim11 <juliankim0911@gmail.com>
2026-03-24 13:04:00 -0300

    Add Navigation System and third-party libs
    Import navigation system feature and required tooling. Adds navigation assets (prefabs, materials, resources), scripts (MapGenerator, MapManager, MapNode, MapView, MapPlayerTracker, DottedLineRenderer, etc.), ScriptableObjects for events/config, and editor inspectors. Also includes third-party libraries and editor utilities (DOTween, OneLine, FlexUnit) and their resources/settings. Updates Gameplay.unity and removes obsolete MapNodeData (and its meta).

--- Assets/Scripts/NavigationSystem/MapManager.cs
+++ Assets/Scripts/NavigationSystem/MapManager.cs
@@ -1,20 +1,62 @@
+﻿using System.Linq;
 using UnityEngine;
+using Newtonsoft.Json;
 
-public class MapManager : MonoBehaviour
+namespace Map
 {
-    public MapNode currentNode;
+    public class MapManager : MonoBehaviour
+    {
+        public MapConfig config;
+        public MapView view;
 
-    public void OnNodeClicked(MapNode selectedNode)
-    {
-        if (currentNode.children.Contains(selectedNode))
+        public Map CurrentMap { get; private set; }
+
+        private void Start()
         {
-            currentNode = selectedNode;
-            EnterRoom(selectedNode.data);
-            //UpdateVisuals();
+            if (PlayerPrefs.HasKey("Map"))
+            {
+                string mapJson = PlayerPrefs.GetString("Map");
+                Map map = JsonConvert.DeserializeObject<Map>(mapJson);
+                // using this instead of .Contains()
+                if (map.path.Any(p => p.Equals(map.GetBossNode().point)))
+                {
+                    // payer has already reached the boss, generate a new map
+                    GenerateNewMap();
+                }
+                else
+                {
+                    CurrentMap = map;
+                    // player has not reached the boss yet, load the current map
+                    view.ShowMap(map);
+                }
+            }
+            else
+            {
+                GenerateNewMap();
+            }
         }
-    }
-    private void EnterRoom(MapNodeData data)
-    {
-        Debug.Log("Enter to: " + data.nodeName);
+
+        public void GenerateNewMap()
+        {
+            Map map = MapGenerator.GetMap(config);
+            CurrentMap = map;
+            Debug.Log(map.ToJson());
+            view.ShowMap(map);
+        }
+
+        public void SaveMap()
+        {
+            if (CurrentMap == null) return;
+
+            string json = JsonConvert.SerializeObject(CurrentMap, Formatting.Indented,
+                new JsonSerializerSettings {ReferenceLoopHandling = ReferenceLoopHandling.Ignore});
+            PlayerPrefs.SetString("Map", json);
+            PlayerPrefs.Save();
+        }
+
+        private void OnApplicationQuit()
+        {
+            SaveMap();
+        }
     }
 }
---
+++ Assets/Scripts/NavigationSystem/NodeBlueprint.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+namespace Map
+{
+    public enum NodeType
+    {
+        FightEvent,
+        EliteFightEvent,
+        RestEvent,
+        TreasureEvent,
+        StoreEvent,
+        FinalEvent,
+        MysteryEvent
+    }
+}
+
+namespace Map
+{
+    [CreateAssetMenu]
+    public class NodeBlueprint : ScriptableObject
+    {
+        public Sprite sprite;
+        public NodeType nodeType;
+    }
+}
